4644fdbe272b65f0059b94912704af2aeb4549ab
Vinicius <saraivinicius@gmail.com>
2023-04-23 16:15:48 -0300

    Initial commit

---
+++ app/src/main/java/daniel/lop/io/marvelappstarter/util/Constants.kt
@@ -0,0 +1,17 @@
+package daniel.lop.io.marvelappstarter.util
+
+object Constants {
+
+    const val DATABASE_NAME = "marvel.db"
+    const val BASE_URL = "https://gateway.marvel.com/v1/public/"
+
+    const val PUBLIC_KEY = "0ab67ff06e0bdd7f03391421287491ae"
+    const val PRIVATE_KEY = "2076a4556180a56776d3d0ba570b6157780d8811"
+
+    const val LAST_SEARCH_QUERY = "last_search_query"
+    const val DEFAULT_QUERY = ""
+
+    const val APIKEY = "apikey"
+    const val HASH = "hash"
+    const val TS = "ts"
+}
